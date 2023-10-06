79ffaac95e59ba3ad2317d31ba5fd549b8bf9c10
e-cesar9 <cesarpetitjean@gmail.com>
2023-10-06 13:34:24 +0200

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import Canvas from './canvas'
+import Customize from './pages/Customize'
+import Home from './pages/Home'
+
+function App() {
+  return (
+    <main className='app transition-all ease-in'>
+      <Home />
+      <Canvas />
+      <Customize />
+    </main>
+    
+  )
+}
+
+export default App
---
+++ client/src/store/index.js
@@ -0,0 +1,17 @@
+import { proxy } from 'valtio';
+
+const state = proxy({
+    intro: true,
+    color: '#EFBD48',
+    isFilterTable: false,
+    isActiveTable: true,
+
+    isLogoTexture: true,
+    isFullTexture: false,
+    logoDecal: './threejs.png',
+    fullDecal: './threejs.png',
+
+})
+
+export default state;
+
